b90c689b95a172fc698f91e5c6a3b820a366b9a2
GiuliaVessoso <153520723+GiuliaVessoso@users.noreply.github.com>
2024-08-03 11:17:17 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Funzione per cambiare slide
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Funzione per mostrare le slide
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
