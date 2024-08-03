53d8bbef565b4e3760d5c22646f93182bf441bd7
GiuliaVessoso <153520723+GiuliaVessoso@users.noreply.github.com>
2024-08-03 12:07:31 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('mobile-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
 
