f26d38e4c536c749856152d408629956350f6d5e
GiuliaVessoso <153520723+GiuliaVessoso@users.noreply.github.com>
2024-09-04 10:53:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,21 @@
-// Seleziona elementi DOM
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('.dropdown');
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    
+    navLinks.classList.toggle('active');
 
-// Funzione per aggiornare l'icona del toggle button
-function updateToggleIcon(isOpen) {
-    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+    // Cambia l'icona da hamburger a X
+    if (navLinks.classList.contains('active')) {
+        menuIcon.innerHTML = '&#10006;'; // X
+    } else {
+        menuIcon.innerHTML = '&#9776;';  // Hamburger (☰)
+    }
+
+    console.log('Menu toggled!');  // Messaggio di controllo per verificare il click
 }
 
-// Gestisci il click sul toggle button
-toggleBtn.onclick = function() {
-    dropDownMenu.classList.toggle('open');
-    const isOpen = dropDownMenu.classList.contains('open');
-    updateToggleIcon(isOpen);
-};
 
-// Gestisci il click sui link del menu a discesa
-const dropdownLinks = document.querySelectorAll('.dropdown a');
-dropdownLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        dropDownMenu.classList.remove('open');
-        updateToggleIcon(false);
-    });
-});
-/ Quando la pagina è caricata, esegui il seguente codice
-        document.addEventListener("DOMContentLoaded", function() {
-            const backToTopBtn = document.getElementById("backToTopBtn");
 
-            // Mostra il bottone quando si scrolla oltre 20px
-            window.onscroll = function() {
-                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                    backToTopBtn.style.display = "block"; // Mostra il bottone
-                } else {
-                    backToTopBtn.style.display = "none"; // Nasconde il bottone
-                }
-            };
-<script>
     document.addEventListener('DOMContentLoaded', function() {
         // Mostra il popup quando la pagina è completamente caricata
         var popup = document.getElementById('discount-popup');
@@ -55,7 +35,7 @@ dropdownLinks.forEach(link => {
             }
         });
     });
-</script>
+
 
           
   
