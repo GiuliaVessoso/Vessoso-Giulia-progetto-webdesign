592ff46dd66b5254d3eaadba2a40608daaa55e3f
GiuliaVessoso <153520723+GiuliaVessoso@users.noreply.github.com>
2024-09-06 18:42:50 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
+// Funzione per aprire/chiudere il menu
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     const menuIcon = document.querySelector('.menu-icon');
     
     navLinks.classList.toggle('active');
 
-       // Cambia l'icona da hamburger a X
-        if (navLinks.classList.contains('active')) {
-            menuIcon.innerHTML = '&#10006;'; // X
-        } else {
-            menuIcon.innerHTML = '&#9776;';  // Hamburger (☰)
-        }
-        
-        console.log('Menu toggled!');  // Messaggio di controllo per verificare il click
+    // Cambia l'icona da hamburger a X
+    if (navLinks.classList.contains('active')) {
+        menuIcon.innerHTML = '&#10006;'; // X
+    } else {
+        menuIcon.innerHTML = '&#9776;';  // Hamburger (☰)
     }
     
-    // Aggiungi l'evento click all'icona del menu
-    const menuIcon = document.querySelector('.menu-icon');
-    if (menuIcon) {
-        menuIcon.addEventListener('click', toggleMenu);
-    }
+    console.log('Menu toggled!');  // Messaggio di controllo per verificare il click
+}
 
-    // Mostra il popup quando la pagina è completamente caricata
+// Aggiungi l'evento click all'icona del menu
+const menuIcon = document.querySelector('.menu-icon');
+if (menuIcon) {
+    menuIcon.addEventListener('click', toggleMenu);
+}
+
+// Mostra il popup quando la pagina è completamente caricata
+window.addEventListener('load', function() {
     var popup = document.getElementById('discount-popup');
     var closeButton = document.querySelector('.close-btn');
     
@@ -42,5 +44,5 @@ function toggleMenu() {
         });
     }
 });
-  
+
 
